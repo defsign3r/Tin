bcbd2dc01e99985a39ca3641e12893acfc01a887
button <defsign3r@gmail.com>
2018-02-20 11:30:49 +0800

    sanjiaowang

---
+++ mytin/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace mytin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(454, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "生成点";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(535, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "生成TIN";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(464, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 88);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(464, 167);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 88);
+            this.listBox2.TabIndex = 3;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 12;
+            this.listBox3.Location = new System.Drawing.Point(464, 280);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 88);
+            this.listBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(462, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "坐标表";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(462, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "边表";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(462, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "结点表";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(462, 371);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "苏科测绘1412戴逸凡 制作";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 389);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ mytin/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace mytin
+{
+    public partial class Form1 : Form
+    {
+        Point[] point = new Point[4];
+        Point[] center_of_a_circle = new Point[4];
+        private Pen pen = new Pen(Color.Black,2);//画笔
+        private Brush brush = Brushes.Red;//画刷
+        Graphics g;//画图
+        Random rand = new Random();//生成随机数
+        int[]x=new int[4];
+        int[]y=new int[4];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)//单击第一个按钮发生事件生成四个随机点并记录在表中
+        {
+            this.Refresh();//清空画布
+            listBox1.Items.Clear();
+            listBox3.Items.Clear();//清除坐标表中的内容
+            for (int i = 0; i < 4; i++)
+            {
+                x[i] = rand.Next(50, 300);
+                y[i] = rand.Next(50, 300);
+                g = this.CreateGraphics();
+                g.FillEllipse(brush, x[i], y[i], 5, 5);
+                listBox1.Items.Add("point"+i+":"+x[i] + "," + y[i]);//输出点坐标
+                point[i].X = x[i];
+                point[i].Y = y[i];
+                g.DrawString("point"+i, new Font("宋体", 13),Brushes.Blue, new PointF(x[i] + 5, y[i] + 5));
+                listBox3.Items.Add("point"+i);
+            }
+        }
+
+        public double Ed(Point a,Point b)//定义了一个通过两端点求线段长度的函数
+        {
+            double ed = System.Math.Sqrt((a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y));
+            return ed;
+        }
+
+        public double R(Point a, Point c, Point d)//定义了一个通过三角形三点求外接圆半径的函数
+        {
+            double ed_ac = System.Math.Sqrt((a.X - c.X) * (a.X - c.X) + (a.Y - c.Y) * (a.Y - c.Y));
+            double ed_ad = System.Math.Sqrt((a.X - d.X) * (a.X - d.X) + (a.Y - d.Y) * (a.Y - d.Y));
+            double ed_cd = System.Math.Sqrt((d.X - c.X) * (d.X - c.X) + (d.Y - c.Y) * (d.Y - c.Y));
+            double p = (ed_ac  + ed_ad  + ed_cd ) / 2;
+            double s = System.Math.Sqrt(p * (p - ed_ac ) * (p - ed_ad ) * (p - ed_cd));
+            double r = ed_ac  * ed_ad  * ed_cd  / (4 * s);
+            return r;
+        }
+
+        public System.Drawing.Point Center_of_a_circle(Point a, Point c, Point d)//定义一个求三角形外接圆圆心的函数
+        {
+            double t1 = a.X * a.X + a.Y * a.Y;
+            double t2 = c.X * c.X + c.Y * c.Y;
+            double t3 = d.X * d.X + d.Y * d.Y; 
+            double ed_ac = System.Math.Sqrt((a.X - c.X) * (a.X - c.X) + (a.Y - c.Y) * (a.Y - c.Y));
+            double ed_ad = System.Math.Sqrt((a.X - d.X) * (a.X - d.X) + (a.Y - d.Y) * (a.Y - d.Y));
+            double ed_cd = System.Math.Sqrt((d.X - c.X) * (d.X - c.X) + (d.Y - c.Y) * (d.Y - c.Y));
+            double temp = a.X * c.Y + c.X * d.Y + d.X * a.Y -a.X * d.Y - c.X * a.Y - d.X * c.Y;
+            double p = (ed_ac + ed_ad + ed_cd) / 2;
+            double s = System.Math.Sqrt(p * (p - ed_ac) * (p - ed_ad) * (p - ed_cd));
+            double x0 = (t2 * d.Y + t1 * c.Y + t3 * a.Y - t2 * a.Y - t3 * c.Y - t1 * d.Y) / temp / 2;
+            double y0 = (t3 * c.X + t2 * a.X + t1 * d.X - t1 * c.X - t2 * d.X - t3 * a.X) / temp / 2; 
+            center_of_a_circle[0].X = (int)x0 ;
+            center_of_a_circle[0].Y = (int)y0 ;
+            return center_of_a_circle[0];
+
+        }
+
+        private void quchongfu(ListBox lb)//一个去除listbox中重复项的函数，参数是目标listbox
+        {
+            
+
+            for (int i = 0; i < lb.Items.Count; i++)
+            {
+                for (int j = i + 1; j < lb.Items.Count; j++)
+                {
+                    if (lb.Items[i].Equals(lb.Items[j]))
+                    {
+                        lb.Items.Remove(lb.Items[j]);
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            g = this.CreateGraphics();
+            if (System.Math.Sqrt((Center_of_a_circle(point[3], point[0], point[2]).X - point[1].X) * (Center_of_a_circle(point[3], point[0], point[2]).X - point[1].X) + (Center_of_a_circle(point[3], point[0], point[2]).Y - point[1].Y) * (Center_of_a_circle(point[3], point[0], point[2]).Y - point[1].Y)) > R(point[3], point[0], point[2]))
+            {
+                g.DrawLine(pen, point[3], point[0]);
+                g.DrawLine(pen, point[3], point[2]);
+                g.DrawLine(pen, point[2], point[0]);
+                listBox2.Items.Add("ed03");
+                listBox2.Items.Add("ed23");
+                listBox2.Items.Add("ed02");
+            }
+
+            if (System.Math.Sqrt((Center_of_a_circle(point[3], point[0], point[1]).X - point[2].X) * (Center_of_a_circle(point[3], point[0], point[1]).X - point[2].X) + (Center_of_a_circle(point[3], point[0], point[1]).Y - point[2].Y) * (Center_of_a_circle(point[3], point[0], point[1]).Y - point[2].Y)) > R(point[3], point[0], point[1]))
+            {
+                g.DrawLine(pen, point[3], point[0]);
+                g.DrawLine(pen, point[3], point[1]);
+                g.DrawLine(pen, point[1], point[0]);
+                listBox2.Items.Add("ed03");
+                listBox2.Items.Add("ed13");
+                listBox2.Items.Add("ed01");
+            }
+
+            if (System.Math.Sqrt((Center_of_a_circle(point[3], point[2], point[1]).X - point[0].X) * (Center_of_a_circle(point[3], point[2], point[1]).X - point[0].X) + (Center_of_a_circle(point[3], point[2], point[1]).Y - point[0].Y) * (Center_of_a_circle(point[3], point[2], point[1]).Y - point[0].Y)) > R(point[3], point[2], point[1]))
+            {
+                g.DrawLine(pen, point[3], point[2]);
+                g.DrawLine(pen, point[3], point[1]);
+                g.DrawLine(pen, point[1], point[2]);
+                listBox2.Items.Add("ed23");
+                listBox2.Items.Add("ed13");
+                listBox2.Items.Add("ed12");
+            }
+
+            if (System.Math.Sqrt((Center_of_a_circle(point[0], point[2], point[1]).X - point[3].X) * (Center_of_a_circle(point[0], point[2], point[1]).X - point[3].X) + (Center_of_a_circle(point[0], point[2], point[1]).Y - point[3].Y) * (Center_of_a_circle(point[0], point[2], point[1]).Y - point[3].Y)) > R(point[0], point[2], point[1]))
+            {
+                g.DrawLine(pen, point[0], point[2]);
+                g.DrawLine(pen, point[0], point[1]);
+                g.DrawLine(pen, point[1], point[2]);
+                listBox2.Items.Add("ed02");
+                listBox2.Items.Add("ed01");
+                listBox2.Items.Add("ed12");
+            }
+            quchongfu(listBox2);//通过去重复函数将边表中的重复项去除掉
+        }           
+    }
+}
